5e168ef62f60c5a7dda8739da467ff2d00c0416b
Evan C. Wright <evancwright@yahoo.com>
2017-01-01 20:24:21 -0600

    initial commit

---
+++ prep_table.asm
@@ -0,0 +1,23 @@
+;prep_table.asm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;prep table
+;1st byte is length number
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+prep_table
+	.db 2
+	.strz "IN"
+	.db 2
+	.strz "AT"
+	.db 2
+	.strz "TO"
+	.db 6
+	.strz "INSIDE"
+	.db 3
+	.strz "OUT"
+	.db 5
+	.strz "UNDER"
+	.db 2
+	.strz "ON"
+	.db 0
+
